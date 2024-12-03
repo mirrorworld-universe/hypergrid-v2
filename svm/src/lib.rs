58a3351023fe6f16b6e6fc65b935bfcf0264110c
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-04 00:31:45 +0800

    refactor: Temporarily remove HashMap and RWLock

--- svm/src/lib.rs
+++ svm/src/lib.rs
@@ -4,7 +4,6 @@ use solana_sdk::{
     pubkey::Pubkey,
 };
 use solana_svm::transaction_processing_callback::TransactionProcessingCallback;
-use std::{collections::HashMap, sync::RwLock};
 
 /*
  * Steps to have a working Runtime SVM
