325fc9aea49694139ce4f40d548a2fe635f0fb47
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-04 00:26:12 +0800

    feat: Finish account migrator with docs

--- svm/src/lib.rs
+++ svm/src/lib.rs
@@ -1,7 +1,10 @@
 use solana_client::rpc_client::RpcClient;
-use solana_sdk::{account::AccountSharedData, pubkey::Pubkey};
+use solana_sdk::{
+    account::{AccountSharedData, ReadableAccount},
+    pubkey::Pubkey,
+};
 use solana_svm::transaction_processing_callback::TransactionProcessingCallback;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::RwLock};
 
 /*
  * Steps to have a working Runtime SVM
@@ -26,18 +29,27 @@ impl AccountMigrator {
 }
 
 impl TransactionProcessingCallback for AccountMigrator {
+    /// Get Account from L1
+    ///
+    /// Hoists L1 accounts into the SVM API representation of an
+    /// [`solana_sdk::account::Account`], an [`AccountSharedData`]
     fn get_account_shared_data(&self, pubkey: &Pubkey) -> Option<AccountSharedData> {
-        None
+        // ReadableAccount Trait enables AccountSharedData to be converted from Account
+        // See: https://docs.rs/solana-account/2.1.4/solana_account/trait.ReadableAccount.html
+        let account: AccountSharedData = self.rpc_client.get_account(pubkey).ok()?.into();
+        Some(account)
     }
 
+    /// Check Ownership
+    ///
+    /// Evaluate if an account is owned by one of the [`Pubkey`]
+    /// listed in the owners slice
+    ///
+    /// Returns
+    /// - Some(usize) (Index of Match)
+    /// - None (No Match)
     fn account_matches_owners(&self, account: &Pubkey, owners: &[Pubkey]) -> Option<usize> {
-        None
+        self.get_account_shared_data(account)
+            .and_then(|account| owners.iter().position(|key| account.owner().eq(key)))
     }
-
-    /*
-     * Optionally, implement
-     */
-    // fn add_builtin_account(&self, _name: &str, _program_id: &Pubkey) {}
-    //
-    // fn inspect_account(&self, _address: &Pubkey, _account_state: AccountState, _is_writable: bool) {}
 }
