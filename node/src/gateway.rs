d95652ee51c103e37986090f543181149b5731ea
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-05 04:20:05 +0800

    feat: Add Solana Gateway

--- node/src/gateway.rs
+++ node/src/gateway.rs
@@ -1,13 +1,10 @@
 use grid_core::GridGateway;
 use grid_rpc::{SolanaRpcPubSubServer, SolanaRpcServer};
-
-pub struct SolanaGatewayRpc;
-
-impl SolanaRpcServer for SolanaGatewayRpc {}
-
-pub struct SolanaGatewayRpcPubSub;
-
-impl SolanaRpcPubSubServer for SolanaGatewayRpcPubSub {}
+use jsonrpsee::{
+    core::{RpcResult, SubscriptionResult},
+    PendingSubscriptionSink,
+};
+use solana_rpc_client_api::config::{RpcSendTransactionConfig, RpcSimulateTransactionConfig};
 
 pub struct SolanaGateway {
     rpc_http: SolanaGatewayRpc,
@@ -21,6 +18,40 @@ impl GridGateway for SolanaGateway {
     }
     async fn start_ws_server(&self) -> Result<()> {
         let rpc = self.rpc_pubsub.into_rpc();
+        Ok(())
+    }
+}
+
+struct SolanaGatewayRpc;
+
+#[jsonrpsee::core::async_trait]
+impl SolanaRpcServer for SolanaGatewayRpc {
+    // --------------------------
+    // Send / Simulate
+    // --------------------------
+
+    async fn send_transaction(
+        &self,
+        transaction: String,
+        config: Option<RpcSendTransactionConfig>,
+    ) -> RpcResult<String> {
+        Ok(String::from(""))
+    }
+
+    async fn simulate_transaction(
+        &self,
+        transaction: String,
+        config: Option<RpcSimulateTransactionConfig>,
+    ) -> RpcResult<String> {
+        Ok(String::from(""))
+    }
+}
+
+struct SolanaGatewayRpcPubSub;
+
+#[jsonrpsee::core::async_trait]
+impl SolanaRpcPubSubServer for SolanaGatewayRpcPubSub {
+    async fn slot_subscribe(&self, pending: PendingSubscriptionSink) -> SubscriptionResult {
         Ok(())
     }
 }
