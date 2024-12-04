23af80de4ace16bcefb949399a3a76e52793d015
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-05 00:42:06 +0800

    feat: Node start handler

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,24 +1,34 @@
+mod cli;
 mod node;
 
 use anyhow::Result;
-use clap::{Arg, ArgAction, Command};
+use clap::{ArgMatches, Command};
+use cli::CliHandler;
 
 fn main() -> Result<()> {
-    let matches = Command::new("grid")
+    let toplevel_matches = Command::new("grid")
         // .version("0.1.0") // commented out to get version from manifest
         .author("Sonic Engineering <engineering@sonic.game>")
         .about("Grid V2")
         .arg_required_else_help(true)
-        .arg(
-            Arg::new("TEST")
-                .help("Test flag")
-                .long("test")
-                .short('t')
-                .action(ArgAction::SetTrue),
-        )
+        .subcommand(Command::new("start").about("Run the Grid node"))
         .get_matches();
 
-    println!("{}", matches.get_flag("TEST"));
+    match toplevel_matches.subcommand() {
+        Some(("start", sub_matches)) => StartHandler::handle_matches(sub_matches)?,
+
+        _ => {
+            // clap handles this case with
+        }
+    }
 
     Ok(())
+}
+
+pub struct StartHandler;
+
+impl CliHandler for StartHandler {
+    fn handle_matches(matches: &ArgMatches) -> Result<()> {
+        Ok(())
+    }
 }
