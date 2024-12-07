c485cd6f399af082e46a9ad562672d66969bb96f
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-07 16:01:01 +0800

    docs: Document design decisions

--- node/solana/rpc/src/lib.rs
+++ node/solana/rpc/src/lib.rs
@@ -1,5 +1,16 @@
 pub mod rpc_http;
 pub mod rpc_pubsub;
 
+// Re-exporting to import all protocol (RPC) dependencies
+// from a single source of truth (this crate)
+//
+// Example
+// ```
+//    use grid_node_solana_rpc::{
+//        jsonrpsee::core::RpcResult,
+//        rpc_http::SolanaRpcServer,
+//        solana_rpc_client_api::config::{RpcSendTransactionConfig, RpcSimulateTransactionConfig},
+//    };
+// ```
 pub use jsonrpsee;
 pub use solana_rpc_client_api;
