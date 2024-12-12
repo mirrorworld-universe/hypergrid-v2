9052aa23651cd8f14543e51a91208e482a6d2ae0
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-12 20:13:57 +0800

    feat: Create Grid SFN

--- node/examples/sfn/README.md
+++
@@ -1,3 +0,0 @@
-# Simple Full Node (SFN)
-
-A full node built to uncover the question marks in protoyping the full Grid node.
--- node/examples/sfn/main.rs
+++ node/examples/sfn/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Examples working");
-}
+use std::sync::Arc;
+use grid_node::*;
+use grid_node_storage::*;
+use grid_node_runtime::*;
+use grid_node_router::*;
+
+fn main() {}
+
+//------------------------------------------
+// Grid
+//------------------------------------------
+
+pub struct Grid(Arc<InnerGrid>);
+
+pub struct InnerGrid {};
