f6fd45c5c805d2b6e804eba75f1d07d44317fd82
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-21 16:38:21 +0800

    feat: Peering starter

--- node/examples/peering/main.rs
+++ node/examples/peering/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello Peering!");
+use anyhow::Result;
+use grid_logger::{initialize_logger, tracing::*};
+use libp2p::{tcp, tls, yamux, SwarmBuilder, Transport};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    initialize_logger(2)?;
+
+    info!("Building new network identity");
+
+    let mut swarm = SwarmBuilder::with_new_identity().with_tokio().with_tcp(
+        tcp::Config::default(),
+        tls::Config::new,
+        yamux::Config::default,
+    )?;
+
+    Ok(())
 }
