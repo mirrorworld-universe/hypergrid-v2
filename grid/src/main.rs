23d88381e187978ff97d6429ae48e0879c586cce
Kristian Quirapas <kmquirapas@gmail.com>
2025-02-28 03:40:00 +0800

    feat: Cli

--- grid/src/main.rs
+++ grid/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, Command};
+#[tokio::main]
+async fn main() {
+    let app_m = Command::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_author!())
+        .about(crate_description!())
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("start")
+                .about("Grid Starter")
+                .arg_required_else_help(true)
+                .arg(Arg::new("MODE").long("MODE").short('m')),
+        )
+        .get_matches();
 }
