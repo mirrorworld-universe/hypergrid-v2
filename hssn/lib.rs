401489923787812bbfcedba7f0c76e316c8f40bd
kquirapas <kmquirapas@gmail.com>
2025-06-17 07:17:53 +0000

    feat: Init HSSN

--- hssn/lib.rs
+++ hssn/lib.rs
@@ -1,1 +1,14 @@
 pub mod error;
+
+pub struct HssnConfig {}
+
+pub struct Hssn {}
+
+impl Hssn {
+    fn with_config(config: HssnConfig) -> Self {
+        Self {}
+    }
+}
+
+#[rpc(server)]
+pub trait HssnRpc {}
